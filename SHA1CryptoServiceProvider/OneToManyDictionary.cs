e0959ef1e28e33413edbe73b531e28871c63f464
Shiv Kumar <shiv.kmr003@gmail.com>
2020-12-27 17:46:45 -0500

    Initial Commit

---
+++ SHA1CryptoServiceProvider/OneToManyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hashing
+{
+    internal sealed class OneToManyDictionary<TKey, TValue> : IEnumerable<KeyValuePair<TKey, HashSet<TValue>>>
+    {
+        private readonly Dictionary<TKey, HashSet<TValue>> _keyValuePairs = new Dictionary<TKey, HashSet<TValue>>();
+
+        public HashSet<TValue> this[TKey key] => _keyValuePairs[key];
+
+        public bool ContainsKey(TKey key)
+        {
+            return _keyValuePairs.ContainsKey(key);
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (_keyValuePairs.TryGetValue(key, out var values))
+            {
+                values.Add(value);
+            }
+            else
+            {
+                values = new HashSet<TValue>();
+                if (value != null)
+                {
+                    values.Add(value);
+                }
+                _keyValuePairs[key] = values;
+            }
+        }
+
+        public IEnumerator<KeyValuePair<TKey, HashSet<TValue>>> GetEnumerator()
+        {
+            return _keyValuePairs.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ SHA1CryptoServiceProvider/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Hashing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personNames = File.ReadAllLines(@"..\..\..\PresonNames.txt");
+            var hashAlogorithm =  SHA512.Create();
+
+            var tablesDictionary = new OneToManyDictionary<string, string>();
+
+            foreach (var personName in personNames)
+            {
+                var hashByes = hashAlogorithm.ComputeHash(Encoding.UTF8.GetBytes(personName));
+                var value = BitConverter.ToInt32(hashByes);
+                var machineId = Math.Abs(value % 5);
+                tablesDictionary.Add(machineId.ToString(), personName);
+            }
+
+            foreach (var machine in tablesDictionary.OrderBy(k => k.Key))
+            {
+                Console.WriteLine($"Machine: {machine.Key}\tCount: {tablesDictionary[machine.Key].Count}");
+            }
+
+            foreach (var personName in personNames)
+            {
+                var hashByes = hashAlogorithm.ComputeHash(Encoding.UTF8.GetBytes(personName));
+                var value = BitConverter.ToInt32(hashByes);
+                var machineId = Math.Abs(value % 5);
+
+                tablesDictionary[machineId.ToString()].Contains(personName);
+            }
+        }
+
+        private static string GetHashValue(byte[] hashBytes)
+        {
+            var sb = new StringBuilder();
+            foreach (var hashByte in hashBytes)
+            {
+                sb.Append(hashByte.ToString("x2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////private static HashAlgorithm GetHashAlgorithm()
+////{
+////    return SHA1.Create();
+////}
+
+////private static string GetHashCode(HashAlgorithm hashAlgorithm, string input)
+////{
+////    var buffer = hashAlgorithm.ComputeHash(Encoding.UTF8.GetBytes(input));
+////    var stringBuilder = new StringBuilder();
+////    for (int i = 0; i < buffer.Length; i++)
+////    {
+////        stringBuilder.Append(buffer[i].ToString("x2"));
+////    }
+
+////    return stringBuilder.ToString();
+////}
+
